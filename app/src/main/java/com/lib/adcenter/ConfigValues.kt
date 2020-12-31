0bd1af7886e04487af937a4ec37f3b5b285f6392
rasool000 <37304191+rasool000@users.noreply.github.com>
2020-12-30 21:23:46 -0800

    v1

--- app/src/main/java/com/lib/adcenter/ConfigValues.kt
+++ app/src/main/java/com/lib/adcenter/ConfigValues.kt
@@ -2,6 +2,6 @@ package com.lib.adcenter
 
 class ConfigValues {
     companion object{
-        const val baseUrl = "https://rasoolmansoory.ir/test.php"
+        const val baseUrl = "https://adcenterofficial.ir/adcenter_bot/install_checker.php"
     }
 }
